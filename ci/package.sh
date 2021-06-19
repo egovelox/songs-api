524e33a57ea03f67ed3b7cb7d458281c7fb5ffe7
cunc <cunc@proxair.local>
2021-06-20 00:25:49 +0200

    [CHORE] add github actions

--- ci/package.sh
+++ ci/package.sh
@@ -4,20 +4,16 @@
 # set -e
 
 cd "$(dirname "$0")/.." || exit 2
-
+echo "Working directory: $(pwd)"
 
 die() { echo "$2" >&2; exit "$1"; }
-run() { ( "$@"; ); ret="$?"; echo This is $ret; (( "$ret" == 0 )) || die 3 "Error while calling '$*' ($ret)"; }
+run() { ( set -x; "$@"; ); ret="$?"; (( "$ret" == 0 )) || die 3 "Error while calling '$*' ($ret)"; }
 ssm() { run aws ssm get-parameter --name "$1" --output text --query "Parameter.Value"; }
 
-echo "Working directory: $(pwd)";
-
-
-SONGS_API_REGION=$(ssm "songs_api_region");
+SONGS_API_REGION=$(ssm "songs_api_region")
 export SONGS_API_REGION
 
-envsubst < ci/test.txt > ci/destination.txt;
-echo "envsubst $?"
-echo "Envsubst finished";
-cat ci/destination.txt;
-echo $?
+envsubst < ci/test.txt > ci/destination.txt
+echo "Envsubst finished"
+
+cat ci/destination.txt
